4d05a4cecdf50d233ac5419e0918c5a83f8c8b9c
Zachary Hillman <zohillman@gmail.com>
2017-04-11 11:16:22 -0400

    Update HelloWorld.java

--- main/java/edu/ma/wa/nqueue/HelloWorld.java
+++ main/java/edu/ma/wa/nqueue/HelloWorld.java
@@ -2,12 +2,14 @@ package edu.ma.wa.nqueue;
 
 public class HelloWorld{
   
+  public String final MESSAGE = "Hello World";
+  
   public static void main(String[] args){
        System.out.println(getMessage());
   }
   
   public static String getMessage(){
-       return "Hello World"; 
+       return MESSAGE; 
   }
   
 }
