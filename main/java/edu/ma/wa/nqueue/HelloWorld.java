dda5d177ce3a0b9a0947fc7d796f45ef82d0a2d3
newmanshee <newman_shee@hotmail.com>
2017-04-11 12:16:44 -0400

    Update HelloWorld.java
    added static

--- main/java/edu/ma/wa/nqueue/HelloWorld.java
+++ main/java/edu/ma/wa/nqueue/HelloWorld.java
@@ -2,7 +2,7 @@ package edu.ma.wa.nqueue;
 
 public class HelloWorld{
   
-  public String final MESSAGE = "Hello World";
+  public final static String MESSAGE = "Hello World";
   
   public static void main(String[] args){
        System.out.println(getMessage());
