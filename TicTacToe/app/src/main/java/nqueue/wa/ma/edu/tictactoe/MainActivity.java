ad2fc45e220ac9dcd21336bb47413bb89955cd23
Josh V <josh.vasilevsky@gmail.com>
2017-04-12 22:27:10 -0400

    Set up Menu

--- TicTacToe/app/src/main/java/nqueue/wa/ma/edu/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/nqueue/wa/ma/edu/tictactoe/MainActivity.java
@@ -1,7 +1,9 @@
 package nqueue.wa.ma.edu.tictactoe;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface title = Typeface.createFromAsset(getAssets(), "PWPerspective.ttf");
+        Typeface buttons = Typeface.createFromAsset(getAssets(), "Cheveuxdange.ttf");
+        TextView titleview = (TextView)findViewById(R.id.textviewtitle);
+        TextView buttonview = (TextView)findViewById(R.id.button);
+        TextView button2view = (TextView)findViewById(R.id.button2);
+        TextView button3view = (TextView)findViewById(R.id.button3);
+        titleview.setTypeface(title);
+        buttonview.setTypeface(buttons);
+        button2view.setTypeface(buttons);
+        button3view.setTypeface(buttons);
     }
 }
