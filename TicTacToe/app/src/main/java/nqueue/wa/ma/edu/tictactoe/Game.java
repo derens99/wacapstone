bcf2d6884887170ac5a975e3124fcf684cf5feeb
andrewH01 <drew.hartnett@gmail.com>
2017-04-12 21:06:57 -0400

    Updated Game class

--- TicTacToe/app/src/main/java/nqueue/wa/ma/edu/tictactoe/Game.java
+++ TicTacToe/app/src/main/java/nqueue/wa/ma/edu/tictactoe/Game.java
@@ -5,4 +5,61 @@ package nqueue.wa.ma.edu.tictactoe;
  */
 
 public class Game {
+
+    private icons[][] board;      //To be changed to Icons once class is created
+    private Player player1;     //Player 1 will always be HumanPlayer
+    private Player player2;     //Player 2 will either be HumanPlayer or AIPlayer
+    private boolean player1turn;
+
+    public enum icons(){
+        EMPTY, X, O;
+    }
+
+    //Default constructor
+    public Game(){
+        board = new icons[3][3];
+        player1 = new Player();
+        player2 = new Player();
+        player1turn = true;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                board[i][j]= EMPTY;
+            }
+        }
+    }
+
+    public boolean makeMove(Move move){
+        if(player1turn){
+            board[move.getX(),move.getY()]=X;
+            if(isWinner(player1)){
+
+            }
+        }
+        else{
+            board[move.getX(),move.getY()]=O;
+            if(isWinner(player2)){
+
+            }
+        }
+        player1turn!=player1turn;
+    }
+
+    public Player[][] getBoard(){
+        return board;
+    }
+
+    public boolean isWinner(Player p){
+        if(checkRows(p)||checkColumns(p)||checkDiagonals(p))
+            return true;
+        return false;
+    }
+
+    public boolean checkRows(Player p){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if()
+            }
+        }
+    }
+
 }
